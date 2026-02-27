92d0ed9a5a0d4b6b88917a747788e0f243a42bfe
Pascal Baljet <pascal@protone.media>
2026-02-27 16:49:49 +0100

    Dist

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
 
 export default defineConfig({
+    build: {
+        minify: false,
+    },
     plugins: [
         dts({
             insertTypesEntry: true,
